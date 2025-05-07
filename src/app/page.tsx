89854aca5ba529ff63032fa8ba10a5c73bb14b89
LeeEugene1 <uwm1004@gmail.com>
2025-05-07 21:13:30 +0900

    chore: vitest 설치 및 설정(vanila extract 연동)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import Counter from "@/app/components/sample/Counter";
 import * as styles from "./page.css";
 export default function Home() {
   return (
     <div className={styles.myStyle}>
-      <h1>🧁 Hello from vanilla-extract!</h1>
+      <h1>Home</h1>
+      <Counter />
     </div>
   );
 }
