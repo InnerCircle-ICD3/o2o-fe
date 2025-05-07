0e5502321992c56a7b849ee1c1aba27665c383cf
SeoJaeWan <sjw7324@igis.co.kr>
2025-05-08 08:27:05 +0900

    chore: api 폴더 명칭 변경

--- src/hooks/useTodos.ts
+++ src/hooks/useTodos.ts
@@ -1,4 +1,4 @@
-import { apiClient } from "@/api/client";
+import { apiClient } from "@/apis/client";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 interface Todo {
