34d96f5553b04066a35abd0eea3221fdab43a0fb
Ing-yeo_Developer <52366178+SeoJaeWan@users.noreply.github.com>
2025-05-08 07:50:28 +0900

    chore: husky 설정 (#13)
    Co-authored-by: LeeEugene1 <uwm1004@gmail.com>
    Co-authored-by: SeoJaeWan <sjw7324@igis.co.kr>

--- src/app/providers/QueryProvider.tsx
+++ src/app/providers/QueryProvider.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 
 export default function QueryProvider({ children }: { children: ReactNode }) {
   const queryClient = new QueryClient();
 
-  return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  );
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 import QueryProvider from "./QueryProvider";
 
 export default function Providers({ children }: { children: ReactNode }) {
