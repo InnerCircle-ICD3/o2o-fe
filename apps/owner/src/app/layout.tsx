52f32176ff3dda457b29899d14a75b25271f5283
SeoJaeWan <sjw7324@igis.co.kr>
2025-05-09 08:30:00 +0900

    chore: shadcn 설치

--- apps/owner/src/app/layout.tsx
+++ apps/owner/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Providers from "@/providers";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 
 if (process.env.NEXT_RUNTIME === "nodejs" && process.env.NODE_ENV === "development") {
   const { worker } = await import("../mocks/server");
--- apps/owner/src/app/page.tsx
+++ apps/owner/src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import Counter from "@/components/sample/Counter";
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="text-3xl font-bold underline">Home</h1>
       <Counter />
+
+      <Button>임시 버튼</Button>
     </div>
   );
 }
