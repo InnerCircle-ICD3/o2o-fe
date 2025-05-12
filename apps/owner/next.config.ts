154b0432cc4f7ede8e324b8690d7b49606706bae
Rachel <borapark13@gmail.com>
2025-05-12 20:50:05 +0900

    fix: next build 경로수정

--- apps/owner/next.config.ts
+++ apps/owner/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  distDir: ".next",
+  output: "standalone",
 };
 
 export default nextConfig;
