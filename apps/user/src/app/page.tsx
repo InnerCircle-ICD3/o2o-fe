78b636c2954a8e3b639d36eea4af75a5be6e5b49
SeoJaeWan <sjw7324@igis.co.kr>
2025-05-14 12:58:46 +0900

    chore: rollback main page

--- apps/user/src/app/page.tsx
+++ apps/user/src/app/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import Counter from "@/components/sample/Counter";
+import * as styles from "./page.css";
 export default function Home() {
   return (
-    <div>
+    <div className={styles.myStyle}>
       <h1>Home</h1>
       <Counter />
     </div>
