01aecb3ec284a2e95f02dc916c19d9b38dd70b8a
sanholl <edgar.jang93@hanmail.net>
2025-05-14 23:09:01 +0900

    feat: 중복 제거 테스트코드 추가

--- apps/user/src/mocks/handlers/recommendations/index.ts
+++ apps/user/src/mocks/handlers/recommendations/index.ts
@@ -1,15 +1,30 @@
-import { http, HttpResponse } from 'msw';
-import { baseUrl } from '../../utils';
+import { http, HttpResponse } from "msw";
+import { baseUrl } from "../../utils";
 
-const mockRecommendations = ['피자', '피자스쿨', '피자나라치킨공주', '피나치공', '피자헛', '피자마루', '피자알볼로', '치킨', '짜장면'];
+const mockRecommendations = [
+  "피자",
+  "피자스쿨",
+  "피자나라치킨공주",
+  "피나치공",
+  "피자헛",
+  "피자마루",
+  "피자알볼로",
+  "치킨",
+  "짜장면",
+  "Pizza",
+  "pizza",
+  "pizza",
+  "PIZZA",
+  "Pizza Hut",
+  "Dominos PIZZA",
+  "pizzaria",
+];
 
 const handlers = [
   http.get(`${baseUrl}/recommendations`, ({ request }) => {
     const url = new URL(request.url);
-    const searchTerm = url.searchParams.get('searchTerm') ?? '';
-    const filtered = mockRecommendations.filter((word) =>
-      word.includes(searchTerm)
-    );
+    const searchTerm = url.searchParams.get("searchTerm") ?? "";
+    const filtered = mockRecommendations.filter((word) => word.includes(searchTerm));
     return HttpResponse.json(filtered);
   }),
 ];
