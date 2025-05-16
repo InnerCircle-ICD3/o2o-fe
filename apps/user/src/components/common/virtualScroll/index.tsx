276307f5b62adc6303310fe2939c815cfeb53fe4
SeoJaeWan <sjw7324@naver.com>
2025-05-16 19:13:52 +0900

    refactor: className & onScrollEnd naming

--- apps/user/src/components/common/virtualScroll/index.tsx
+++ apps/user/src/components/common/virtualScroll/index.tsx
@@ -2,6 +2,7 @@
 
 import type { HeightSpec, VirtualItemProps } from "@/types/virtualScroll.type";
 import { renderVirtualContent } from "@/utils/virtualScroll";
+import classNames from "classnames";
 import { Children, useEffect, useRef, useState } from "react";
 import type { PropsWithChildren, ReactElement } from "react";
 import * as style from "./virtualScroll.css";
@@ -9,10 +10,10 @@ import * as style from "./virtualScroll.css";
 interface VirtualScrollProps extends PropsWithChildren {
   overscan?: number;
   heights: Record<string, HeightSpec>;
-  onBottom?: () => void;
+  onScrollEnd?: () => void;
 }
 
-const VirtualScroll = ({ overscan = 2, heights, children, onBottom }: VirtualScrollProps) => {
+const VirtualScroll = ({ overscan = 2, heights, children, onScrollEnd }: VirtualScrollProps) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const observerRef = useRef<HTMLDivElement>(null);
   const [scrollTop, setScrollTop] = useState(0);
@@ -28,6 +29,8 @@ const VirtualScroll = ({ overscan = 2, heights, children, onBottom }: VirtualScr
     scrollTop,
     overscan,
   });
+
+  const containerStyle = classNames(style.container, "virtual-scroll");
 
   // resize 감지
   useEffect(() => {
@@ -57,8 +60,8 @@ const VirtualScroll = ({ overscan = 2, heights, children, onBottom }: VirtualScr
     const observer = new IntersectionObserver(
       (entries) => {
         for (const entry of entries) {
-          if (entry.isIntersecting && onBottom) {
-            onBottom();
+          if (entry.isIntersecting && onScrollEnd) {
+            onScrollEnd();
           }
         }
       },
@@ -68,17 +71,17 @@ const VirtualScroll = ({ overscan = 2, heights, children, onBottom }: VirtualScr
     observer.observe(observerRef.current);
 
     return () => observer.disconnect();
-  }, [onBottom]);
+  }, [onScrollEnd]);
 
   return (
-    <div ref={containerRef} className={`virtual-scroll ${style.container}`}>
+    <div ref={containerRef} className={containerStyle}>
       <div style={{ height: totalHeight }}>
         {containerSize.height !== 0 && containerSize.width !== 0 && (
           <div style={{ transform: `translateY(${translateY}px)` }}>{visible}</div>
         )}
       </div>
 
-      {onBottom && <div ref={observerRef} />}
+      {onScrollEnd && <div ref={observerRef} />}
     </div>
   );
 };
