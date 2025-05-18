47aacc2247227ba8e36e97a445f97990249f4bc9
Rachel <borapark13@gmail.com>
2025-05-18 10:29:02 +0900

    chore: add husky scripts for pre-commit setup (#69)

--- apps/user/src/mocks/handlers/index.ts
+++ apps/user/src/mocks/handlers/index.ts
@@ -1,4 +1,3 @@
 import exampleHandlers from "./example";
 import recommendationsHandlers from "./recommendations";
-
 export const handlers = [...exampleHandlers, ...recommendationsHandlers];
--- apps/user/src/styles/theme.css.ts
+++ apps/user/src/styles/theme.css.ts
@@ -62,7 +62,6 @@ export const theme = createGlobalTheme(":root", globalTheme, {
       base: "#999999",
       light: "#CCCCCC",
     },
-
     label: {
       sales: {
         background: "#D8F4E4",
