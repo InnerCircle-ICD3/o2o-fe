8531d7032bbe87e20432bc16d9e2c5d6a1f2e92f
SeoJaeWan <sjw7324@naver.com>
2025-05-19 21:12:10 +0900

    feat: my order

--- apps/user/src/app/(topNav)/my-orders/[id]/page.tsx
+++ apps/user/src/app/(topNav)/my-orders/[id]/page.tsx
@@ -1,5 +1,23 @@
-const Page = () => {
-  return <>Page</>;
+import { getOrderDetail } from "@/apis/ssr/orders";
+import OrderInfo from "@/components/common/orderInfo";
+import ActionButtons from "@/components/ui/my-orders/actionButtons";
+
+interface PageProps {
+  params: Promise<{ id: string }>;
+}
+
+const Page = async (props: PageProps) => {
+  const { params } = props;
+  const { id } = await params;
+
+  const orderDetail = await getOrderDetail(id);
+
+  return (
+    <div>
+      <OrderInfo orderDetail={orderDetail} />
+      <ActionButtons orderDetail={orderDetail} />
+    </div>
+  );
 };
 
 export default Page;
---
+++ apps/user/src/constants/my-orders/index.ts
@@ -0,0 +1,5 @@
+export const ORDER_STATUS = {
+  pending: "PENDING" as const,
+  completed: "COMPLETED" as const,
+  canceled: "CANCELED" as const,
+};
