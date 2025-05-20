702656bb5c81d6f189dce9a90abdc1835df07423
이유진 <yjlee@fimasia.co.kr>
2025-05-20 22:21:32 +0900

    init

--- apps/user/src/app/(bottomNav)/page.tsx
+++ apps/user/src/app/(bottomNav)/page.tsx
@@ -1,10 +1,21 @@
-"use client";
-
+import { StoreCard } from "@/components/ui/storeList/storeCard";
+import { productTitle } from "@/styles/common.css";
 import * as styles from "./page.css";
-export default function Home() {
+
+export default async function Home() {
   return (
     <div className={styles.myStyle}>
-      <h1>Home</h1>
+      <h2 className={productTitle}>우리동네에서 지금 할인중이에요!</h2>
+      <StoreCard
+        imageUrl="/assets/dessert.jpg"
+        title="남남 디저트"
+        subtitle="김밥 / 주먹밥 / 가정식"
+        originalPrice={10000}
+        salePrice={5000}
+        rating={4.7}
+        reviews={257}
+        distance="1km"
+      />
     </div>
   );
 }
