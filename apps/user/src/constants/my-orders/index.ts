688bb622b7158d41c3e8a3c082da302cafa40baf
Ing-yeo_Developer <52366178+SeoJaeWan@users.noreply.github.com>
2025-05-20 22:09:03 +0900

    feat: 주문 내역 확인 (#75)
    Co-authored-by: SeoJaeWan <sjw7324@igis.co.kr>

---
+++ apps/user/src/constants/my-orders/index.ts
@@ -0,0 +1,5 @@
+export const ORDER_STATUS = {
+  pending: "PENDING",
+  completed: "COMPLETED",
+  canceled: "CANCELED",
+};
--- apps/user/src/types/apis/order.type.ts
+++ apps/user/src/types/apis/order.type.ts
@@ -1,7 +1,10 @@
+import type { ORDER_STATUS } from "@/constants/my-orders";
 import type { Product, StoreDetail } from "./store.type";
 
+export type OrderStatus = (typeof ORDER_STATUS)[keyof typeof ORDER_STATUS];
 export interface OrderDetail {
   orderId: number;
   store: StoreDetail;
   products: Product[];
+  status: OrderStatus;
 }
