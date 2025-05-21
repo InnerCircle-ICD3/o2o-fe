b72c4eb99bc077ec1542f0e8c487f690108c5bb3
LeeEugene1 <uwm1004@gmail.com>
2025-05-21 22:37:36 +0900

    design: 메인 1차

--- apps/user/src/app/(bottomNav)/page.css.ts
+++ apps/user/src/app/(bottomNav)/page.css.ts
@@ -1,11 +1,11 @@
 import { style } from "@vanilla-extract/css";
 
 export const myStyle = style({
-  // cast to pixels
-  padding: 10,
-  marginTop: 25,
+	// cast to pixels
+	padding: 10,
+	marginTop: 25,
 
-  // unitless properties
-  flexGrow: 1,
-  opacity: 0.5,
+	// unitless properties
+	flexGrow: 1,
+	// opacity: 0.5,
 });
--- apps/user/src/app/(bottomNav)/page.tsx
+++ apps/user/src/app/(bottomNav)/page.tsx
@@ -1,13 +1,37 @@
 import { StoreCard } from "@/components/ui/storeList/storeCard";
 import { productTitle } from "@/styles/common.css";
 import * as styles from "./page.css";
+import React, { useEffect, useState } from "react";
+import { useInfiniteScroll } from "@/hooks/useInfiniteScroll";
+
+interface Store {
+  id: number;
+  name: string;
+}
 
 export default async function Home() {
+  const size = 10;
+
+  const { data, isFetching, error } = useInfiniteScroll({
+    size,
+    api: "/api/stores",
+  });
+
+  const [storeList, setStoreList] = useState<Store[]>([]);
+
+  useEffect(() => {
+    if (data) {
+      setStoreList((prev) => [...prev, ...data.contents]);
+    }
+  }, [data]);
+
+  if (error) return <div>Error: {error.message}</div>;
+
   return (
     <div className={styles.myStyle}>
       <h2 className={productTitle}>우리동네에서 지금 할인중이에요!</h2>
       <StoreCard
-        imageUrl="/assets/dessert.jpg"
+        imageUrl="/images/store_1.png"
         title="남남 디저트"
         subtitle="김밥 / 주먹밥 / 가정식"
         originalPrice={10000}
