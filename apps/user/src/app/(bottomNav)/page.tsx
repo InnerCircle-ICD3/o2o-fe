612d2e960dc1a0dffe102f1a1d38c5db8a636756
LeeEugene1 <uwm1004@gmail.com>
2025-05-24 16:31:03 +0900

    design: 메인2차와 스켈레톤로더UI

--- apps/user/src/app/(bottomNav)/page.tsx
+++ apps/user/src/app/(bottomNav)/page.tsx
@@ -1,45 +1,13 @@
-import { StoreCard } from "@/components/ui/storeList/storeCard";
-import { productTitle } from "@/styles/common.css";
+"use client";
+
+import { productTitle } from "@/components/ui/storesDetail/storesProducts/storesProduct.css";
 import * as styles from "./page.css";
-import React, { useEffect, useState } from "react";
-import { useInfiniteScroll } from "@/hooks/useInfiniteScroll";
-
-interface Store {
-  id: number;
-  name: string;
-}
-
-export default async function Home() {
-  const size = 10;
-
-  const { data, isFetching, error } = useInfiniteScroll({
-    size,
-    api: "/api/stores",
-  });
-
-  const [storeList, setStoreList] = useState<Store[]>([]);
-
-  useEffect(() => {
-    if (data) {
-      setStoreList((prev) => [...prev, ...data.contents]);
-    }
-  }, [data]);
-
-  if (error) return <div>Error: {error.message}</div>;
-
+import StoreListContainer from "./stores/StoreListContainer";
+export default function Home() {
   return (
     <div className={styles.myStyle}>
       <h2 className={productTitle}>우리동네에서 지금 할인중이에요!</h2>
-      <StoreCard
-        imageUrl="/images/store_1.png"
-        title="남남 디저트"
-        subtitle="김밥 / 주먹밥 / 가정식"
-        originalPrice={10000}
-        salePrice={5000}
-        rating={4.7}
-        reviews={257}
-        distance="1km"
-      />
+      <StoreListContainer />
     </div>
   );
 }
