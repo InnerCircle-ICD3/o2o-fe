6d3d68bcf74ccdbb884e1dfc97364bdde9ce7201
SeoJaeWan <sjw7324@naver.com>
2025-05-28 22:20:41 +0900

    feat: mypage notice error

--- apps/user/src/app/(topNav)/mypage/notice/[id]/noticeDetail.css.ts
+++ apps/user/src/app/(topNav)/mypage/notice/[id]/noticeDetail.css.ts
@@ -1,11 +1,21 @@
+import { globalTheme } from "@/styles/theme.css";
 import { style } from "@vanilla-extract/css";
+
+export const errorWrapper = style({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  width: "100%",
+  height: "100%",
+  gap: 16,
+});
 
 export const title = style({
   fontSize: 20,
   fontWeight: "700",
 
   marginTop: 24,
-  marginBottom: 16,
 });
 
 export const content = style({
@@ -14,4 +24,12 @@ export const content = style({
   whiteSpace: "pre-wrap",
 
   paddingBottom: 24,
+});
+
+export const date = style({
+  fontSize: 14,
+  textAlign: "right",
+  color: globalTheme.color.gray.base,
+
+  marginBottom: 16,
 });
