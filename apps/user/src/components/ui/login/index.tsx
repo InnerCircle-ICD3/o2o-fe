86fbee4882ab956175e305f81ac7a8fa6a8ddf36
sanholl <edgar.jang93@hanmail.net>
2025-05-28 22:56:16 +0900

    feat: 유저 로그인 API 연동

--- apps/user/src/components/ui/login/index.tsx
+++ apps/user/src/components/ui/login/index.tsx
@@ -1,15 +1,37 @@
+"use client";
+
+import { SOCIAL_PROVIDERS } from "@/constants/login";
 import Image from "next/image";
 import * as styles from "./login.css";
 
+type Provider = keyof typeof SOCIAL_PROVIDERS;
+
 export default function Login() {
+  const handleSocialLogin = async (provider: Provider) => {
+    const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+    if (!baseUrl) {
+      console.error("환경변수 NEXT_PUBLIC_API_URL이 설정되지 않았습니다.");
+      return;
+    }
+
+    window.location.href = `${baseUrl}/oauth2/authorization/${provider}`;
+  };
+
   return (
     <div className={styles.loginContainer}>
       <Image src="/images/character3.png" alt="loading" width={172} height={136} />
       <Image src="/images/logoTitle.png" alt="loading" width={147} height={62} />
-      <button type="button" className={styles.kakaoButton}>
-        <Image src="/icons/kakao_icon.svg" alt="loading" width={20} height={20} />
-        카카오 로그인
-      </button>
+      {Object.entries(SOCIAL_PROVIDERS).map(([provider, { label, iconSrc }]) => (
+        <button
+          key={provider}
+          type="button"
+          className={styles.kakaoButton}
+          onClick={() => handleSocialLogin(provider as Provider)}
+        >
+          <Image src={iconSrc} alt={`${label} 아이콘`} width={20} height={20} />
+          {label}
+        </button>
+      ))}
     </div>
   );
 }
