9e02e283cca97f024717a41bc0f2e75dd8e850aa
Rachel <borapark13@gmail.com>
2025-05-31 15:37:40 +0900

    refactor: 지역 필터 개선

--- apps/user/src/app/(bottomNav)/ui/mainHeader/index.tsx
+++ apps/user/src/app/(bottomNav)/ui/mainHeader/index.tsx
@@ -1,14 +1,19 @@
 "use client";
 
-import BottomSheet from "@/components/common/bottomSheet";
-import Button from "@/components/common/button";
 import { useBottomSheet } from "@/hooks/useBottomSheet";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import LocationFilter from "./locationFilter";
 import * as styles from "./mainHeader.css";
 
 export default function MainHeader() {
   const { showBottomSheet, handleShowBottomSheet, handleCloseBottomSheet } = useBottomSheet();
+  const [selectedLocation, setSelectedLocation] = useState<string | undefined>();
+
+  const handleLocationChange = (location?: string) => {
+    setSelectedLocation(location);
+  };
 
   return (
     <>
@@ -19,7 +24,9 @@ export default function MainHeader() {
           type="button"
         >
           <Image src="/icons/store.svg" alt="store" width={24} height={24} />
-          <h1 className={styles.mainTitle}>모든 지역의 가게</h1>
+          <h1 className={styles.mainTitle}>
+            {selectedLocation ? `${selectedLocation}의 가게` : "모든 지역의 가게"}
+          </h1>
         </button>
         <div className={styles.mainHeaderRight}>
           <button type="button">
@@ -34,30 +41,11 @@ export default function MainHeader() {
         </div>
       </header>
 
-      <BottomSheet
-        type="shadow"
-        isShow={showBottomSheet.has("location")}
-        title="지역을 선택해주세요"
+      <LocationFilter
+        isOpen={showBottomSheet.has("location")}
+        onChange={handleLocationChange}
         onClose={() => handleCloseBottomSheet("location")}
-      >
-        <ul className={styles.bottomSheetListStyle}>
-          <li className={styles.bottomSheetListItemStyle}>
-            <button className={styles.bottomSheetListItemButtonStyle} type="button">
-              <Image src="/icons/pin.svg" alt="store" width={30} height={30} />
-              <span>강남역</span>
-            </button>
-          </li>
-          <li className={styles.bottomSheetListItemStyle}>
-            <button className={styles.bottomSheetListItemButtonStyle} type="button">
-              <Image src="/icons/add.svg" alt="store" width={30} height={30} />
-              <span>자주가는 장소 등록</span>
-            </button>
-          </li>
-        </ul>
-        <Button status="primary" style={{ marginTop: 20 }}>
-          선택완료
-        </Button>
-      </BottomSheet>
+      />
     </>
   );
 }
