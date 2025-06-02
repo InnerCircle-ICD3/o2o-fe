9289066e73f56713f317040333d5869fa7e24c11
Rachel <borapark13@gmail.com>
2025-06-02 23:29:49 +0900

    feat: 사용자 location 정보 전역변수로 변경 (#109)

--- apps/user/src/stores/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { useSearchHistoryStore } from "./index";
-
-describe("stores index", () => {
-  it("useSearchHistoryStore를 export 한다", () => {
-    expect(useSearchHistoryStore).toBeDefined();
-  });
-});
