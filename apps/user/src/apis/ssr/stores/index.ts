2e964f64fd3dedeb7fa3e77ec280e227fad01c64
Rachel <borapark13@gmail.com>
2025-06-02 23:26:09 +0900

    fix: store list 호출시 location queryString으로 보내줌

--- apps/user/src/apis/ssr/stores/index.ts
+++ apps/user/src/apis/ssr/stores/index.ts
@@ -1,6 +1,6 @@
 import { apiClient } from "@/apis/client";
 import { toSafeResult } from "@/apis/utils/result";
-import type { Product, StoreSearchResponse, StoresDetail } from "@/types/apis/stores.type";
+import type { Product, StoreListResponse, StoresDetail } from "@/types/apis/stores.type";
 
 export const getStoresDetail = async (id: string) => {
   return await toSafeResult(() => apiClient.get<StoresDetail>(`stores/${id}`));
@@ -10,14 +10,12 @@ export const getStoresDetailProducts = async (id: string) => {
   return await toSafeResult(() => apiClient.get<Product[]>(`stores/${id}/products`));
 };
 
-export const getStoreList = async (size: number, page: number) => {
-  return await toSafeResult(() =>
-    apiClient.get<StoreSearchResponse>(`store/list?size=${size}&page=${page}`),
-  );
+export const getStoreList = async (params: string) => {
+  return await toSafeResult(() => apiClient.get<StoreListResponse>(`store/list?${params}`));
 };
 
 export const getSearchStoreList = async (keyword: string, size: number, page: number) => {
   return await toSafeResult(() =>
-    apiClient.get<StoreSearchResponse>(`search/store?keyword=${keyword}&size=${size}&page=${page}`),
+    apiClient.get<StoreListResponse>(`search/store?keyword=${keyword}&size=${size}&page=${page}`),
   );
 };
