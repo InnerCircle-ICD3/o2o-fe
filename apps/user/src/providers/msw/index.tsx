5cf4af70055d189346f26eb6c2e235fcad0a262a
Rachel <borapark13@gmail.com>
2025-06-02 21:05:34 +0900

    fix: msw 요청 조건 수정
    - local, dev 를 구분하게 되면서 node_env로 체크하는 로직을 삭제함
    - node_env 는 전부 development 환경으로 인식하기 때문

--- apps/user/src/providers/msw/index.tsx
+++ apps/user/src/providers/msw/index.tsx
@@ -6,10 +6,7 @@ import { type PropsWithChildren, Suspense, use } from "react";
 const mockingEnabledPromise =
   typeof window !== "undefined"
     ? import("@/mocks/browser").then(async ({ worker }) => {
-        if (
-          process.env.NODE_ENV === "development" ||
-          process.env.NEXT_PUBLIC_VERCEL_MSW_ENV === "development"
-        ) {
+        if (process.env.NEXT_PUBLIC_VERCEL_MSW_ENV === "development") {
           await worker.start();
           worker.use(...handlers);
           module.hot?.dispose(() => {
