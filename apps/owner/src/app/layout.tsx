47948eff5eb6a8156d595ec2b0279c11a0eade04
LeeEugene1 <uwm1004@gmail.com>
2025-06-07 15:46:19 +0900

    fix: header 폴더 이동

--- apps/owner/src/app/layout.tsx
+++ apps/owner/src/app/layout.tsx
@@ -2,7 +2,7 @@ import Providers from "@/providers";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { LayoutContent } from "@/components/layout-content";
+import { LayoutContent } from "@/components/common/layout-content";
 
 if (process.env.NEXT_RUNTIME === "nodejs" && process.env.NODE_ENV === "development") {
   const { worker } = await import("../mocks/server");
