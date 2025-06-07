76e37d703c43f64c3c55507dc5d7ee13e5cc665a
Ing-yeo_Developer <52366178+SeoJaeWan@users.noreply.github.com>
2025-06-07 16:43:18 +0900

    feat: 자주 묻는 질문 (#125)

---
+++ apps/user/src/app/(topNav)/mypage/notice/page.test.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import Page from "./page";
+
+describe("Notice Page", () => {
+  it("페이지가 렌더링된다", () => {
+    const { container } = render(<Page />);
+
+    expect(container).toBeTruthy();
+  });
+});
