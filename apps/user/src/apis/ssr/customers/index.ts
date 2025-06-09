ab1f140b27485ab031db3f5d9f57e0c083db22ea
sanholl <96857799+sanholl@users.noreply.github.com>
2025-06-09 21:32:57 +0900

    feat: 로그인 상태 기반 사용자 정보 전역 관리 및 페이지 접근 제한 처리 (#123)
    - 토큰에 담긴 사용자 정보를 전역으로 관리(userId 등)
    - 점주, 유저 각각 userInfoStore, ownerInfoStore를 이용해서 토큰에 있는 값 사용 가능
    - 페이지 접근 제한 처리
    - 접근 제한 화면 제작

---
+++ apps/user/src/apis/ssr/customers/index.ts
@@ -0,0 +1,13 @@
+import { apiClient } from "@/apis/client";
+import { toSafeResult } from "@/apis/utils/result";
+import type { Customer } from "@/types/apis/accounts.type";
+
+export const patchCustomer = async (customerId: number, nickname: string) => {
+  return await toSafeResult(() =>
+    apiClient.patch(`customers/modify?customerId=${customerId}`, { nickname }),
+  );
+};
+
+export const getCustomer = async (customerId: number) => {
+  return await toSafeResult(() => apiClient.get<Customer>(`customers/me?customerId=${customerId}`));
+};
