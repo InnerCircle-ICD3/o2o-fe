5a70a228a4865f9c19c0abd62d9c8492314de860
sanholl <96857799+sanholl@users.noreply.github.com>
2025-06-11 01:39:26 +0900

    Feat: 유저 로그인 이후 추가 정보 입력 (#124)
    닉네임 등록 페이지 구현
    topNav 오류 수정

---
+++ apps/user/src/components/ui/mypage/completeProfile/index.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { patchCustomer as defaultPatchCustomer } from "@/apis/ssr/customers";
+import Button from "@/components/common/button";
+import { userInfoStore as defaultUseUserStore } from "@/stores/userInfoStore";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import * as styles from "./completeProfile.css";
+
+export default function CompleteProfile({
+  useUserStore = defaultUseUserStore,
+  patchCustomer = defaultPatchCustomer,
+}: {
+  useUserStore?: typeof defaultUseUserStore;
+  patchCustomer?: typeof defaultPatchCustomer;
+} = {}) {
+  const { user } = useUserStore();
+  const [nickname, setNickname] = useState("");
+
+  const router = useRouter();
+
+  const handleCompleteProfile = async () => {
+    if (!user?.customerId) return;
+    const result = await patchCustomer(user.customerId, nickname);
+
+    if (result.success) {
+      router.push("/");
+    }
+  };
+
+  return (
+    <div className={styles.completeProfileContainer}>
+      <input
+        className={styles.input}
+        type="text"
+        placeholder="닉네임을 입력하세요"
+        value={nickname}
+        onChange={(e) => setNickname(e.target.value)}
+      />
+      <Button status="primary" onClick={handleCompleteProfile}>
+        등록하기
+      </Button>
+    </div>
+  );
+}
