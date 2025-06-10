1b46aae9f3bb30e5290ca5f7074ab08501437755
SeoJaeWan <sjw7324@naver.com>
2025-06-10 21:43:52 +0900

    feat: my order api 연동

--- apps/user/src/app/(bottomNav)/my-orders/page.tsx
+++ apps/user/src/app/(bottomNav)/my-orders/page.tsx
@@ -3,7 +3,6 @@
 import ErrorUi from "@/components/common/errorUi";
 import VirtualScroll, { VirtualItem } from "@/components/common/virtualScroll";
 import OrderItem from "@/components/ui/my-orders/orderItem";
-import RequireLogin from "@/components/ui/my-orders/requireLogin";
 import SkeletonStoreCard from "@/components/ui/storeList/storeCard/skeletonStoreCard";
 import { ORDER_STATUS } from "@/constants/my-orders";
 import useGetMyOrder from "@/hooks/api/useGetMyOrder";
@@ -11,24 +10,27 @@ import { userInfoStore } from "@/stores/userInfoStore";
 import * as style from "./myOrders.css";
 
 const Page = () => {
-  const { data: orderDetails, error, isError, isLoading, fetchNextPage } = useGetMyOrder(1);
-
   const { user } = userInfoStore();
   const isLogin = !!user;
+  const { data: orders, error, isError, isLoading, fetchNextPage } = useGetMyOrder(1, !isLogin);
 
-  if (!isLogin) {
-    return <RequireLogin text="주문 내역" />;
-  }
+  // if (!isLogin) {
+  //   return <RequireLogin text="주문 내역" />;
+  // }
 
   if (isError) {
     return <ErrorUi message={error?.message} />;
   }
 
+  const contents = orders?.pages.flatMap((page) => (page.success ? page.data.contents : [])) || [];
+
   return (
     <div className={style.container}>
       <h2 className={style.title}>나의 주문 내역</h2>
       {isLoading ? (
-        <SkeletonStoreCard imagePosition="right" />
+        <SkeletonStoreCard imagePosition="left" />
+      ) : contents.length === 0 ? (
+        <ErrorUi type={"order"} message="아직 주문하신 상품이 없어요" />
       ) : (
         <VirtualScroll
           overscan={3}
@@ -37,27 +39,23 @@ const Page = () => {
               aspectRatio: 388 / 171,
             },
             "order-item-completed": {
-              aspectRatio: 388 / 241,
+              aspectRatio: 388 / 230,
             },
           }}
           onScrollEnd={fetchNextPage}
         >
-          {orderDetails?.pages.map((page) =>
-            page.success
-              ? page.data.contents.map((order) => (
-                  <VirtualItem
-                    key={order.id}
-                    name={
-                      ORDER_STATUS[order.status] === ORDER_STATUS.COMPLETED
-                        ? "order-item-completed"
-                        : "order-item"
-                    }
-                  >
-                    <OrderItem order={order} />
-                  </VirtualItem>
-                ))
-              : null,
-          )}
+          {contents.map((order) => (
+            <VirtualItem
+              key={order.id}
+              name={
+                ORDER_STATUS[order.status] === ORDER_STATUS.DONE
+                  ? "order-item-completed"
+                  : "order-item"
+              }
+            >
+              <OrderItem order={order} />
+            </VirtualItem>
+          ))}
         </VirtualScroll>
       )}
     </div>
--- apps/user/src/types/apis/order.type.ts
+++ apps/user/src/types/apis/order.type.ts
@@ -6,18 +6,23 @@ export interface OrderItem {
   id: number;
   productId: number;
   productName: string;
-  price: number;
+  originPrice: number;
+  finalPrice: number;
   quantity: number;
+  imageUrl: number;
 }
 export interface OrderDetail {
   id: number;
+  storeName: string;
   orderNumber: number;
   customerId: number;
   storeId: number;
   status: OrderStatus;
   orderItems: OrderItem[];
-  createdAt: string;
-  updatedAt: string;
+  canceledAt: string;
+  readiedAt: string;
+  doneAt: string;
+  hasReview: boolean;
 }
 
 export interface OrderList {
