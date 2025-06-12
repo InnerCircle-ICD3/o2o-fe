1e85baa400c30a0fe175751a9119f9e6e7c816eb
Rachel <borapark13@gmail.com>
2025-06-13 00:18:42 +0900

    feat: 주문 연동 (#121)

--- apps/user/src/components/common/products/index.tsx
+++ apps/user/src/components/common/products/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import * as style from "./products.css";
 
 import StatusLabel from "@/components/common/statusLabel";
-import type { Product } from "@/types/apis/stores.type";
+import type { Product, ProductStatus } from "@/types/apis/stores.type";
 import { formatCurrency } from "@/utils/format";
 import generateProductStatus from "@/utils/productStatus";
 import classNames from "classnames";
@@ -23,32 +23,38 @@ const Products = (props: ProductsProps) => {
 
   return (
     <ul className={style.container}>
-      {products.map((product) => {
-        const { status, label } = generateProductStatus(product.inventory.quantity);
-        return (
-          <li key={product.id} className={style.wrapper}>
-            <div className={style.thumbnail}>
-              <Image src={PRODUCTS_IMAGE[product.size]} alt={""} width={88} height={105} />
+      {products
+        .filter((product) => product.status !== "INACTIVE")
+        .map((product) => {
+          const { uiStatus, label } = generateProductStatus<ProductStatus>(
+            product.status,
+            product.inventory,
+          );
+          return (
+            <li key={product.id} className={style.wrapper}>
+              <div className={style.thumbnail}>
+                <Image src={PRODUCTS_IMAGE[product.size]} alt={""} width={88} height={105} />
 
-              {status === "soldOut" && <div className={style.shadowLabel} />}
+                {(uiStatus === "soldOut" || product.inventory.quantity === 0) && (
+                  <div className={style.shadowLabel} />
+                )}
 
-              <div className={style.productLabel}>
-                <StatusLabel status={status}>{label}</StatusLabel>
+                <div className={style.productLabel}>
+                  <StatusLabel status={uiStatus}>{label}</StatusLabel>
+                </div>
               </div>
-            </div>
 
-            <div className={style.infoWrapper}>
-              <h3 className={style.strongText}>{product.name}</h3>
-              <p className={style.subText}>{product.description}</p>
-              <p className={style.subText}>{product.foodType.join(", ")}</p>
-              <p className={classNames(style.subText, globalStyle.middleStroke)}>
-                {formatCurrency(product.price.originalPrice)}
-              </p>
-              <p className={style.strongText}>{formatCurrency(product.price.finalPrice)}</p>
-            </div>
-          </li>
-        );
-      })}
+              <div className={style.infoWrapper}>
+                <h3 className={style.strongText}>{product.name}</h3>
+                <p className={style.subText}>{product.description}</p>
+                <p className={classNames(style.subText, globalStyle.middleStroke)}>
+                  {formatCurrency(product.price.originalPrice)}
+                </p>
+                <p className={style.strongText}>{formatCurrency(product.price.finalPrice)}</p>
+              </div>
+            </li>
+          );
+        })}
     </ul>
   );
 };
