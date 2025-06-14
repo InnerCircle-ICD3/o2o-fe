49828a8b5a705d63750690e2d1da87bf9b5cbc95
sanholl <edgar.jang93@hanmail.net>
2025-06-14 17:09:13 +0900

    chore: type error 해결 - Next.js 15.1 버전의 업데이트로 페이지 컴포넌트에게 Props로 전달되는 serachParams의 타입이 Promise로 변경

--- apps/user/src/app/(topNav)/locations/store-location/page.tsx
+++ apps/user/src/app/(topNav)/locations/store-location/page.tsx
@@ -1,8 +1,13 @@
 import StoreMap from "@/components/ui/locations/storeMap";
 
-export default function Page({ searchParams }: { searchParams: { lat: string; lng: string } }) {
-  const lat = Number(searchParams.lat);
-  const lng = Number(searchParams.lng);
+type PageProps = {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export default async function Page({ searchParams }: PageProps) {
+  const params = await searchParams;
+  const lat = Number(params.lat);
+  const lng = Number(params.lng);
 
   return <StoreMap lat={lat} lng={lng} />;
 }
