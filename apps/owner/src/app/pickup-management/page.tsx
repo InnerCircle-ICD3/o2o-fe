cd390ca551e5e06e4034b6b44c8ef1e8c536dec3
Rachel <borapark13@gmail.com>
2025-06-17 23:40:40 +0900

    feat: 픽업 페이지

--- apps/owner/src/app/pickup-management/page.tsx
+++ apps/owner/src/app/pickup-management/page.tsx
@@ -1,34 +1,54 @@
 "use client";
+
+import { getOrders } from "@/apis/ssr/orders";
 import { Card, CardContent } from "@/components/ui/card";
 import {
   Pagination,
   PaginationContent,
   PaginationItem,
+  PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination";
 import {
   Table,
   TableBody,
-  // biome-ignore lint/correctness/noUnusedImports: <explanation>
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { useState } from "react";
+import useGetOwnerStore from "@/hooks/api/useGetOwnerStore";
+import { useOwnerStore } from "@/stores/ownerInfoStore";
+import type { Order } from "@/types/order";
+import { useEffect, useState } from "react";
 
-// biome-ignore lint/correctness/noUnusedVariables: <explanation>
 const ITEMS_PER_PAGE = 10;
 
-export default function LuckyBagList() {
-  // biome-ignore lint/correctness/noUnusedVariables: <explanation>
+export default function PickupManagementList() {
   const [page, setPage] = useState(1);
-  // const [orderList, setOrderList] = useState<Order[]>([]);
-  // const router = useRouter();
+  const [orderList, setOrderList] = useState<Order[]>([]);
+  const { owner } = useOwnerStore();
 
-  // const totalPages = Math.ceil(products.length / ITEMS_PER_PAGE);
+  const { data: storeData } = useGetOwnerStore(owner?.userId);
 
-  // const paginatedData = products.slice((page - 1) * ITEMS_PER_PAGE, page * ITEMS_PER_PAGE);
+  useEffect(() => {
+    const fetchOrders = async () => {
+      if (!storeData?.id) return;
+      const result = await getOrders(storeData.id);
+      console.log(result, "order!!!<<<<<<");
+      if (result.success) {
+        console.log(result.data);
+        setOrderList(result.data.contents);
+      } else {
+        console.error("주문 조회 실패:", result);
+      }
+    };
+    fetchOrders();
+  }, [storeData?.id]);
+
+  const totalPages = Math.ceil(orderList.length / ITEMS_PER_PAGE);
+
+  const paginatedData = orderList.slice((page - 1) * ITEMS_PER_PAGE, page * ITEMS_PER_PAGE);
 
   return (
     <div className="p-4">
@@ -47,32 +67,14 @@ export default function LuckyBagList() {
               </TableRow>
             </TableHeader>
             <TableBody>
-              {/*{paginatedData.map((bag) => (*/}
-              {/*  <TableRow key={bag.id} onClick={() => router.push(`/product-management/${bag.id}`)}>*/}
-              {/*    <TableCell className="font-semibold">{bag.name}</TableCell>*/}
-              {/*    <TableCell className="text-muted-foreground">*/}
-              {/*      {bag.price.originalPrice.toLocaleString()}원*/}
-              {/*    </TableCell>*/}
-              {/*    <TableCell>{bag.price.discountRate.toLocaleString()}원</TableCell>*/}
-              {/*    <TableCell>{bag.inventory.quantity}개</TableCell>*/}
-              {/*    <TableCell>{bag.size}</TableCell>*/}
-              {/*    <TableCell className="text-right space-x-2">*/}
-              {/*      <Button variant="destructive" size="sm">*/}
-              {/*        수정*/}
-              {/*      </Button>*/}
-              {/*      <Button*/}
-              {/*        variant="outline"*/}
-              {/*        size="sm"*/}
-              {/*        onClick={(e) => {*/}
-              {/*          e.stopPropagation();*/}
-              {/*          handleDelete(bag.id);*/}
-              {/*        }}*/}
-              {/*      >*/}
-              {/*        삭제*/}
-              {/*      </Button>*/}
-              {/*    </TableCell>*/}
-              {/*  </TableRow>*/}
-              {/*))}*/}
+              {paginatedData.map((order) => (
+                <TableRow key={order.orderId}>
+                  {/*<TableCell>{order.orderId}</TableCell>*/}
+                  {/*<TableCell>{order.orderItems[0].productName}</TableCell>*/}
+                  {/*<TableCell>{order.customer.name}</TableCell>*/}
+                  <TableCell>{order.orderStatus}</TableCell>
+                </TableRow>
+              ))}
             </TableBody>
           </Table>
 
@@ -83,10 +85,10 @@ export default function LuckyBagList() {
                   <PaginationPrevious onClick={() => setPage((p) => Math.max(p - 1, 1))} />
                 </PaginationItem>
                 <PaginationItem className="px-4 py-1 text-sm">
-                  {/*{page} / {totalPages}*/}
+                  {page} / {totalPages}
                 </PaginationItem>
                 <PaginationItem>
-                  {/*<PaginationNext onClick={() => setPage((p) => Math.min(p + 1, totalPages))} />*/}
+                  <PaginationNext onClick={() => setPage((p) => Math.min(p + 1, totalPages))} />
                 </PaginationItem>
               </PaginationContent>
             </Pagination>
