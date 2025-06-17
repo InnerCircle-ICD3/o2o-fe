d8495ed84700ae9eccaf30e2bd92aaa115c71a6e
sanholl <edgar.jang93@hanmail.net>
2025-06-17 23:16:52 +0900

    chore: local 설정 수정

--- apps/user/src/providers/index.tsx
+++ apps/user/src/providers/index.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import type { ReactNode } from "react";
+import MswProvider from "./msw";
 import QueryProvider from "./tanstackQuery";
 
 export default function Providers({ children }: { children: ReactNode }) {
-  return <QueryProvider>{children}</QueryProvider>;
+  return (
+    <QueryProvider>
+      <MswProvider>{children}</MswProvider>
+    </QueryProvider>
+  );
 }
