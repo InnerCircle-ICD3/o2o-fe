0eda68a3c15b351c07d0d70c5014136b19e68908
SeoJaeWan <sjw7324@igis.co.kr>
2025-06-20 12:57:59 +0900

    fix: test code

--- apps/user/src/components/ui/my-orders/actionButtons/actionButton.test.tsx
+++ apps/user/src/components/ui/my-orders/actionButtons/actionButton.test.tsx
@@ -20,15 +20,28 @@ describe("ActionButtons Test", () => {
     cleanup();
   });
 
-  it("status가 pending이면 버튼 2개가 보여야 한다", () => {
+  it("status가 READY이면 주문 취소 버튼만 보여야 한다", () => {
     render(<ActionButtons orderDetail={{ ...baseOrder, status: "READY" }} />);
 
     expect(screen.getByRole("button", { name: "주문 취소" })).toBeInTheDocument();
-    expect(screen.getByRole("button", { name: "픽업 완료" })).toBeInTheDocument();
   });
 
-  it("status가 pending이 아니면 아무것도 렌더링되지 않아야 한다", () => {
+  it("status가 CONFIRMED이면 주문 취소 버튼과 픽업 완료 버튼이 보여야 한다", () => {
+    render(<ActionButtons orderDetail={{ ...baseOrder, status: "CONFIRMED" }} />);
+
+    expect(screen.queryByRole("button", { name: "픽업 완료" })).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "주문 취소" })).toBeInTheDocument();
+  });
+
+  it("status가 DONE이 아니면 아무것도 렌더링되지 않아야 한다", () => {
     render(<ActionButtons orderDetail={{ ...baseOrder, status: "DONE" }} />);
+
+    expect(screen.queryByRole("button", { name: "주문 취소" })).not.toBeInTheDocument();
+    expect(screen.queryByRole("button", { name: "픽업 완료" })).not.toBeInTheDocument();
+  });
+
+  it("status가 CANCELED이 아니면 아무것도 렌더링되지 않아야 한다", () => {
+    render(<ActionButtons orderDetail={{ ...baseOrder, status: "CANCELED" }} />);
 
     expect(screen.queryByRole("button", { name: "주문 취소" })).not.toBeInTheDocument();
     expect(screen.queryByRole("button", { name: "픽업 완료" })).not.toBeInTheDocument();
