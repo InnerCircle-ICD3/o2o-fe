e2b9fb1e03ae602702c8bebcaf39979d4f492ed3
Rachel <borapark13@gmail.com>
2025-06-21 13:52:06 +0900

    fix: storeId

--- apps/owner/src/components/common/order-sse-listener.tsx
+++ apps/owner/src/components/common/order-sse-listener.tsx
@@ -1,12 +1,12 @@
 "use client";
+import useGetOwnerStore from "@/hooks/api/useGetOwnerStore";
 import { useOrderModalStore } from "@/stores/orderModalStore";
-import { useOwnerStore } from "@/stores/ownerInfoStore";
 import { useEventSource, useEventSourceListener } from "@react-nano/use-event-source";
 
 export default function OrderSseListener() {
   const openModal = useOrderModalStore((state) => state.openModal);
-  const owner = useOwnerStore((state) => state.owner);
-  const storeId = owner?.userId;
+  const { data: storeData } = useGetOwnerStore();
+  const storeId = storeData?.id;
 
   // EventSource 연결: url, 옵션으로 withCredentials 전달 가능
   const [eventSource, _] = useEventSource(
