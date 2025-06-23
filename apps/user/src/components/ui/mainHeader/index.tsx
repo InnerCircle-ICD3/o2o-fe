a150806db612c67a46973bfd0d3711c167992b98
Ing-yeo_Developer <52366178+SeoJaeWan@users.noreply.github.com>
2025-06-23 21:13:31 +0900

    fix: location filter (#225)

--- apps/user/src/components/ui/mainHeader/index.tsx
+++ apps/user/src/components/ui/mainHeader/index.tsx
@@ -30,7 +30,7 @@ export default function MainHeader() {
         >
           <Image src="/icons/store.svg" alt="store" width={24} height={24} />
           <h1 className={styles.mainTitle}>
-            {search ? search : location ? `${location}의 가게` : "모든 지역의 가게"}
+            {search ? search : location ? `${location}의 가게` : "내 위치의 가게"}
           </h1>
         </button>
         <div className={styles.mainHeaderRight}>
