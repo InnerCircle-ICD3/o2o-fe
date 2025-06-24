ab74838f0c78cee8a9d3d5a75152f343fdf32f82
SeoJaeWan <sjw7324@igis.co.kr>
2025-06-24 14:40:19 +0900

    fix: order

--- apps/user/src/hooks/api/usePostOrder/index.ts
+++ apps/user/src/hooks/api/usePostOrder/index.ts
@@ -15,8 +15,9 @@ const usePostOrder = () => {
         router.push(`/orders/${res.data.id}`);
       },
       onError: (error) => {
-        if (error.errorCode === "O005") {
-          const data = error?.data as { orderIds: number[] };
+        console.log(error.code, error.data);
+        if (error.code === "O005") {
+          const data = error.data as { orderIds: number[] };
           const order = data.orderIds[0];
 
           return router.push(`/orders/${order}`);
