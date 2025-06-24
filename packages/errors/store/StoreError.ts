e7403af9d5b07776c1d7d920b35ae972dda88b31
sanholl <96857799+sanholl@users.noreply.github.com>
2025-06-24 21:37:07 +0900

    chore: 에러 메세지 오류 수정 (#269)

--- packages/errors/store/StoreError.ts
+++ packages/errors/store/StoreError.ts
@@ -6,6 +6,8 @@ export enum StoreErrorCode {
   STORE_UNAVAILABLE = "S003",
   SUBSCRIPTION_NOT_FOUND = "S004",
   SUBSCRIPTION_UPDATE_FAILED = "S005",
+  STORE_COUNT_EXCEEDED = "S018",
+  STORE_PRODUCT_NOT_ENOUGH = "ST004",
 }
 
 export class StoreError extends BaseError {
@@ -15,6 +17,8 @@ export class StoreError extends BaseError {
     [StoreErrorCode.STORE_UNAVAILABLE]: "이용 불가능한 매장입니다.",
     [StoreErrorCode.SUBSCRIPTION_NOT_FOUND]: "구독 정보를 찾을 수 없습니다.",
     [StoreErrorCode.SUBSCRIPTION_UPDATE_FAILED]: "구독 정보 업데이트에 실패했습니다.",
+    [StoreErrorCode.STORE_PRODUCT_NOT_ENOUGH]: "재고가 없어 매장을 오픈할 수 없습니다",
+    [StoreErrorCode.STORE_COUNT_EXCEEDED]: "매장 등록 개수 제한을 초과했습니다. (최대 1개)",
   };
 
   constructor(code: StoreErrorCode, statusCode = 500) {
@@ -39,5 +43,13 @@ export class StoreError extends BaseError {
 
   static subscriptionUpdateFailed(): StoreError {
     return new StoreError(StoreErrorCode.SUBSCRIPTION_UPDATE_FAILED, 500);
+  }
+
+  static storeProductNotFound(): StoreError {
+    return new StoreError(StoreErrorCode.STORE_PRODUCT_NOT_ENOUGH, 409);
+  }
+
+  static storeCountExceeded(): StoreError {
+    return new StoreError(StoreErrorCode.STORE_COUNT_EXCEEDED, 409);
   }
 }
