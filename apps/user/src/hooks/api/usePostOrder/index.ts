ff7c3e361a718e960607993b38e69ba018e8faa5
SeoJaeWan <sjw7324@igis.co.kr>
2025-06-24 10:41:54 +0900

    fix: rollback

--- apps/user/src/hooks/api/usePostOrder/index.ts
+++ apps/user/src/hooks/api/usePostOrder/index.ts
@@ -16,7 +16,7 @@ const usePostOrder = () => {
       },
       onError: (error) => {
         console.log(error);
-        if (error.errorCode === "O005") {
+        if (error.code === "O005") {
           const data = error?.data as { orderIds: number[] };
           const order = data.orderIds[0];
 
--- packages/errors/base/BaseError.ts
+++ packages/errors/base/BaseError.ts
@@ -1,7 +1,6 @@
 export interface ErrorJson {
   name: string;
   code: string;
-  errorCode: string;
   message: string;
   statusCode: number;
   timestamp: Date;
@@ -10,14 +9,12 @@ export interface ErrorJson {
 
 export abstract class BaseError extends Error {
   public readonly code: string;
-  public readonly errorCode: string;
   public readonly statusCode: number;
   public readonly timestamp: Date;
 
   constructor(code: string, message: string, statusCode = 500) {
     super(message);
     this.code = code;
-    this.errorCode = code;
     this.statusCode = statusCode;
     this.timestamp = new Date();
     this.name = this.constructor.name;
@@ -28,7 +25,6 @@ export abstract class BaseError extends Error {
     return {
       name: this.name,
       code: this.code,
-      errorCode: this.errorCode,
       message: this.message,
       statusCode: this.statusCode,
       timestamp: this.timestamp,
