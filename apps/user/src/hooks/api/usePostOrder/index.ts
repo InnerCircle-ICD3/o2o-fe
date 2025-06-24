0237725644958bf63511d88aa02e9474bb4bcba8
SeoJaeWan <sjw7324@igis.co.kr>
2025-06-24 13:25:57 +0900

    chore: check log

--- apps/user/src/hooks/api/usePostOrder/index.ts
+++ apps/user/src/hooks/api/usePostOrder/index.ts
@@ -15,7 +15,7 @@ const usePostOrder = () => {
         router.push(`/orders/${res.data.id}`);
       },
       onError: (error) => {
-        console.error("Order submission error:", error.data);
+        console.error("Order submission error:", error.response);
         console.error("Order submission error:", error.errorCode);
         if (error.errorCode === "O005") {
           const data = error?.data as { orderIds: number[] };
