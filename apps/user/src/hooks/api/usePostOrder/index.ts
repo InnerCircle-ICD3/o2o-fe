3450cf43822992093be1890d50197863d8b5054d
Ing-yeo_Developer <52366178+SeoJaeWan@users.noreply.github.com>
2025-06-24 13:13:23 +0900

    Fix/order (#254)
    Co-authored-by: SeoJaeWan <sjw7324@igis.co.kr>

--- apps/user/src/hooks/api/usePostOrder/index.ts
+++ apps/user/src/hooks/api/usePostOrder/index.ts
@@ -15,13 +15,14 @@ const usePostOrder = () => {
         router.push(`/orders/${res.data.id}`);
       },
       onError: (error) => {
+        console.error("Order submission error:", error);
+
         if (error.errorCode === "O005") {
           const data = error?.data as { orderIds: number[] };
           const order = data.orderIds[0];
 
           return router.push(`/orders/${order}`);
         }
-        console.error("Order submission failed:", error);
       },
     });
   };
